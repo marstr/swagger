8786d96528fdb8839c21fd829930f060673a1af7
Martin Strobel <marstr@microsoft.com>
2017-05-31 13:22:38 -0700

    Adding copy of petstore.json for testing.
    Petstore is the canonical example of using Swagger and is hosted here:
    http://petstore.swagger.io/v2/swagger.json
    
    This test is relatively rudimentary, and only ensure no errors are
    returned as part of parsing this document. Validation of what was parsed
    is not included in this changeset.

---
+++ swagger_test.go
@@ -0,0 +1,31 @@
+package swagger_test
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"github.com/marstr/swagger"
+)
+
+func TestSwagger_UnmarshalJSON(t *testing.T) {
+	testcases := []string{
+		filepath.Join("testdata", "petstore.json"),
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc, func(t *testing.T) {
+			contents, err := ioutil.ReadFile(tc)
+			if err != nil {
+				t.Error(err)
+			}
+
+			var unmarshaled swagger.Swagger
+			err = json.Unmarshal(contents, &unmarshaled)
+			if err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
